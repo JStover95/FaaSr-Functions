b1fe03fd9db586c515d5c2f7f4f6f146005b1a77
Ashish-Ramrakhiani <ramrakhiyaniashish@gmail.com>
2025-10-06 19:34:47 -0700

    Add vm poll

--- FaaSr_py/builtin_functions/__init__.py
+++ FaaSr_py/builtin_functions/__init__.py
@@ -5,8 +5,10 @@ These are framework-provided functions automatically available.
 
 from .vm_start import vm_start
 from .vm_stop import vm_stop
+from .vm_poll import vm_poll
 
 __all__ = [
     'vm_start',
-    'vm_stop'
+    'vm_stop',
+    'vm_poll'
 ]
